1870cb46be9b64d043d76cf4f6b4856991c890f3
Amol Patil <amolsp777@live.com>
2024-06-28 16:16:23 -0500

    Update join-ad.sh

--- linux/join-ad.sh
+++ linux/join-ad.sh
@@ -7,7 +7,8 @@ AD_PASSWORD=$3
 
 # Install required packages
 sudo apt-get update
-sudo apt-get install -y realmd sssd adcli krb5-user packagekit
+# sudo apt-get install -y realmd sssd adcli krb5-user packagekit
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y realmd sssd adcli krb5-user packagekit
 
 # Enable and start necessary services
 sudo systemctl enable sssd
