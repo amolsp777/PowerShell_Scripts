f4c579dd4dfe118b70b0b750d229f0ba7741ffb1
Amol Patil <amolsp777@live.com>
2024-06-28 16:40:47 -0500

    Update join-ad.sh

--- linux/join-ad.sh
+++ linux/join-ad.sh
@@ -7,7 +7,8 @@ AD_PASSWORD=$3
 
 # Install required packages
 sudo apt-get update
-# sudo apt-get install -y realmd sssd adcli krb5-user packagekit
+#### sudo apt-get install -y realmd sssd adcli krb5-user packagekit
+#--- DEBIAN_FRONTEND=noninteractive is required to eliminate the popup window while installing the package.
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y realmd sssd adcli krb5-user packagekit
 
 # Enable and start necessary services
