f86e97cd54acaf30ed01a3d09d5069cce159e6bb
Amol Patil <amolsp777@live.com>
2024-07-01 13:08:34 -0500

    Create test.sh

---
+++ linux/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "------------Step 1----------------"
+df -h
+
+echo "------------Step 2----------------"
+cd /tmp
+ls
+echo "------------Step 3----------------"
+echo "all looks good"
